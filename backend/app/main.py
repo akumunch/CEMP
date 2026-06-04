909301173b79b7f119624cb5225ef262a1dc496d
Akshath Thoguparthy <thoguparthy@gmail.com>
2026-06-04 09:40:06 +0530

    set up fastapi and health check

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Club Chef API",
+    description="Backend services for the Club Event Management Platform",
+    version="1.0.0"
+)
+
+# configuring CORS (cross origin resouce-sharing)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Next.js local development port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", tags=["Health"]) #tags is for grouping the end points together in Swagger ui
+async def health_check():
+    return {
+        "status": "healthy",
+        "message": "Welcome to the Club Chef API Platform"
+    }
